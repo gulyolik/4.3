14d653191854d3dcbf966e2132eb99843cd233ff
gulyolik <olia.guliaeva@yandex.ru>
2022-10-16 17:30:21 +0300

    Fixes

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(double credit, double percent, double months) {
         double oneMonth = percent / 12 / 100;
-        double y = 1 + oneMonth;
-        double coefficient = Math.pow(y, months) * oneMonth / (Math.pow(y, months) - 1);
+        double degree = 1 + oneMonth;
+        double coefficient = Math.pow(degree, months) * oneMonth / (Math.pow(degree, months) - 1);
         double result = credit * coefficient;
         return (int) result;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        service.calculate (1000000,  9.99, 12);
-        double payment = service.calculate(1000000,  9.99, 12) ;
-        System.out.println ("При сумме кредита в 1000.000 рублей сроком на 1 год ежемесячный аннуитетный платёж составит:  " + payment);
+        service.calculate(1000000, 9.99, 12);
+        double payment = service.calculate(1000000, 9.99, 12);
+        System.out.println("При сумме кредита в 1000.000 рублей сроком на 1 год ежемесячный аннуитетный платёж составит:  " + payment);
 
         service.calculate(1000000, 9.99, 24);
         payment = service.calculate(1000000, 9.99, 24);
